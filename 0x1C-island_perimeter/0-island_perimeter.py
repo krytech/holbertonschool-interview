4efd4bc2df82b9708a425c1b92319f051f5b984f
Carson Stearn <cds@carson-mb1.verizon.net>
2022-11-18 16:04:37 -0500

    files

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Function that returns the perimeter of the island
+described in grid.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    result = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i - 1 < 0 or grid[i - 1][j] == 0:
+                    result += 1
+                if grid[i + 1][j] or i + 1 >= len(grid) == 0:
+                    result += 1
+                if j - 1 < 0 or grid[i][j - 1] == 0:
+                    result += 1
+                if grid[i][j + 1] == 0 or j + 1 >= len(grid[i]):
+                    result += 1
+
+    return result
