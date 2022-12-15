20e9f8af7a6d1862b7aac5590d6c393b9a39b47c
Carson Stearn <cds@carson-mb1.verizon.net>
2022-12-15 18:51:44 -0500

    more cases

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -8,17 +8,22 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-	if (*str == 0 && *pattern == 0)
-		return (1);
+    int period = 0;
+    int star = 0;
 
-	if (*str == *pattern || *pattern == '.')
-		return (regex_match(str + 1, pattern + 1));
+    if (!str || !pattern)
+        return (0);
 
-	if (*pattern == '*')
-	{
-		if (*str == 0)
-			return (regex_match(str, pattern + 1));
-		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 1));
-	}
-	return (0);
+    period = *str && (*str == *pattern || *pattern == '.');
+    star = *(pattern + 1) == '*';
+
+    if (!*str && !star)
+        return (*pattern ? 0 : 1);
+    else if (period && star)
+        return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+    else if (period && !star)
+        return (regex_match(str + 1, pattern + 1));
+    else if (star)
+        return (regex_match(str, pattern + 2));
+    return (0);
 }
